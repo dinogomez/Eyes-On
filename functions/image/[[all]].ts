b667c47cfe3b3755d0905e6d2219fd7d80158297
dinogomez <dinogomez117@gmail.com>
2025-03-08 20:07:49 +0800

    test: img

--- functions/image/[[all]].ts
+++ functions/image/[[all]].ts
@@ -1,30 +1,24 @@
-import type { R2Bucket } from "@cloudflare/workers-types";
 
 interface Env {
+  // @ts-ignore
   BUCKET: R2Bucket;
 }
 
 // @ts-ignore
-export async function onRequestGet(context) {
-  const path = new URL(context.request.url).pathname.replace("/images/", "");
-  console.log(path);
-  const file = await context.env.BUCKET.get(path);
-  
-  if (!file) {
-    return new Response(null, { 
-      status: 404,
-      headers: {
-        'Cache-Control': 'public, max-age=0, must-revalidate'
-      }
-    });
+export async function onRequestGet(request: Request, env: Env, ctx: ExecutionContext) {
+
+  const url = new URL(request.url);
+  const key = url.pathname.replace("/image/", "");
+
+  const object = await env.BUCKET.get(key);
+  if (!object) {
+    return new Response(null, { status: 404 });
   }
 
   const headers = new Headers();
-  // Cache successful responses for 1 year
-  headers.set('Cache-Control', 'public, max-age=31536000, immutable');
-  headers.set('Content-Type', file.httpMetadata?.contentType || 'application/octet-stream');
-  
-  return new Response(file.body, {
-    headers
-  });
+  object.writeHttpMetadata(headers);
+  headers.set('etag', object.httpEtag);
+  headers.set("Content-Type", object.httpMetadata?.contentType || "application/octet-stream");
+
+  return new Response(object.body, { status: 200, headers });
 } 
